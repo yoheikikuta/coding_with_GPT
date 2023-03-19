9e4ac3f21fef052b2e378fbd8d4045d9e4c6e1cd
yoheikikuta <diracdiego@gmail.com>
2023-03-19 16:00:22 +0900

    Add arXiv paper categories with hard-coding

--- arxiv_paper_fetcher_app.py
+++ arxiv_paper_fetcher_app.py
@@ -4,7 +4,18 @@ from datetime import datetime
 
 
 def get_inputs():
-    category = st.text_input("Category (e.g. hep-ph):")
+    categories = [
+        "hep-ph",
+        "hep-th",
+        "astro-ph",
+        "cs.AI",
+        "cs.CL",
+        "math.ST",
+        "q-bio",
+        "quant-ph",
+    ]
+    
+    category = st.selectbox("Category", categories)
     num_results = st.number_input("Number of papers to fetch", min_value=1, max_value=50, value=10, step=1)
     start_year = st.number_input("Start year for submitted date range", min_value=1900, max_value=datetime.now().year, value=1900, step=1)
     end_year = st.number_input("End year for submitted date range", min_value=1900, max_value=datetime.now().year, value=datetime.now().year, step=1)
