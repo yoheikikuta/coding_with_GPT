22e234bb200f4999bcfb498d45e8cfa4f2ec91bb
yoheikikuta <diracdiego@gmail.com>
2023-03-19 11:51:13 +0900

    Refactoring

--- arxiv_paper_fetcher.py
+++ arxiv_paper_fetcher.py
@@ -12,25 +12,32 @@ def search_arxiv(query, max_results=10):
 
     return response.entries
 
-def main(query, max_results=10, start_year=None, end_year=None, keyword=None):
-    if start_year and end_year:
-        query += f" AND submittedDate:[{start_year}0101 TO {end_year}1231]"
+
+def print_paper_info(paper, idx):
+    print(f"{idx}. {paper.title}")
+    print(f"著者: {', '.join(author.name for author in paper.authors)}")
+    print(f"arXiv ID: {paper.id}")
+    print(f"要約: {paper.summary[:200]}...")  # 要約の最初の200文字を表示
+    print(f"公開日: {paper.published}")
+    print(f"更新日: {paper.updated}\n")
+
+
+def main(args):
+    query = f"cat:{args.category}"
+    
+    if args.start_year and args.end_year:
+        query += f" AND submittedDate:[{args.start_year}0101 TO {args.end_year}1231]"
     else:
         query += f" AND submittedDate:[{datetime.now().year}0101 TO {datetime.now().year}1231]"
         
-    if keyword:
-        query += f' AND (ti:"{keyword}" OR abs:"{keyword}")'
+    if args.keyword:
+        query += f' AND (ti:"{args.keyword}" OR abs:"{args.keyword}")'
 
-    max_results = 10  # 取得したい最大結果数
-    papers = search_arxiv(query, max_results)
+    papers = search_arxiv(query, args.num_results)
 
     for idx, paper in enumerate(papers, start=1):
-        print(f"{idx}. {paper.title}")
-        print(f"著者: {', '.join(author.name for author in paper.authors)}")
-        print(f"arXiv ID: {paper.id}")
-        print(f"要約: {paper.summary[:200]}...")  # 要約の最初の200文字を表示
-        print(f"公開日: {paper.published}")
-        print(f"更新日: {paper.updated}\n")
+        print_paper_info(paper, idx)
+
 
 def get_args():
     parser = argparse.ArgumentParser(description="Fetch arXiv papers with specified category and keyword in title or abstract.")
@@ -44,5 +51,4 @@ def get_args():
 
 if __name__ == "__main__":
     args = get_args()
-    query = f"cat:{args.category}"
-    main(query, args.num_results, args.start_year, args.end_year, args.keyword)
+    main(args)
